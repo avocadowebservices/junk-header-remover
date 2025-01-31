e8d4d2efc85f5271fef6fd5177e7f846b635c9f2
AvocadoWeb Services <129627395+avocadowebservices@users.noreply.github.com>
2025-01-31 12:59:14 -0500

    Update junk-header-remover.php
    updated and changes some coding lines

--- junk-header-remover.php
+++ junk-header-remover.php
@@ -2,21 +2,31 @@
 /*
 Plugin Name: Junk Header Remover for WordPress
 Plugin URI: https://avocadoweb.net
-Description: Removes unnecessary clutter junk from WP Header
-Version: 1.0.0
+Description: Removes unnecessary clutter from the WP Header.
+Version: 1.0.1
 Author: Joseph Brzezowski
+Author URI: https://avocadoweb.net
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-/*Removes RSD, XMLRPC, WLW, WP Generator, ShortLink, Comment Feed links and Many more*/
-remove_action('wp_head', 'rsd_link'); // remove really simple discovery link
-remove_action('wp_head', 'wp_generator'); // remove wordpress version
-remove_action('wp_head', 'feed_links', 2); // remove rss feed links (make sure you add them in yourself if youre using feedblitz or an rss service)
-remove_action('wp_head', 'feed_links_extra', 3); // removes all extra rss feed links
-remove_action('wp_head', 'index_rel_link'); // remove link to index page
-remove_action('wp_head', 'wlwmanifest_link'); // remove wlwmanifest.xml (needed to support windows live writer)
-remove_action('wp_head', 'start_post_rel_link', 10, 0); // remove random post link
-remove_action('wp_head', 'parent_post_rel_link', 10, 0); // remove parent post link
-remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0); // remove the next and previous post links
-remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
-remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0 );
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+function aw_remove_junk_headers() {
+    remove_action('wp_head', 'rsd_link'); // Remove Really Simple Discovery link
+    remove_action('wp_head', 'wp_generator'); // Remove WordPress version
+    remove_action('wp_head', 'feed_links', 2); // Remove RSS feed links
+    remove_action('wp_head', 'feed_links_extra', 3); // Remove extra RSS feed links
+    remove_action('wp_head', 'index_rel_link'); // Remove index page link
+    remove_action('wp_head', 'wlwmanifest_link'); // Remove wlwmanifest.xml (Windows Live Writer)
+    remove_action('wp_head', 'start_post_rel_link', 10, 0); // Remove random post link
+    remove_action('wp_head', 'parent_post_rel_link', 10, 0); // Remove parent post link
+    remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0); // Remove adjacent post links
+    remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+    remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+}
+add_action('init', 'aw_remove_junk_headers');
+
 ?>
